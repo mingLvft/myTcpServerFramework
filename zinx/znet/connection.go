fc6df5971d4becbab956e1859e633ebc6201c901
mingLvft <843547212@qq.com>
2023-12-21 02:12:22 +0800

    集成消息封装机制

--- zinx/znet/connection.go
+++ zinx/znet/connection.go
@@ -1,8 +1,9 @@
 package znet
 
 import (
+	"errors"
 	"fmt"
-	"myServerDemo/zinx/utils"
+	"io"
 	"myServerDemo/zinx/ziface"
 	"net"
 )
@@ -51,13 +52,39 @@ func (c *Connection) StartReader() {
 
 	for {
 		// 读取客户端的数据到buf中，最大512字节
-		buf := make([]byte, utils.GlobalObject.MaxPackageSize)
-		//cnt, err := c.Conn.Read(buf)
-		_, err := c.Conn.Read(buf)
-		if err != nil {
-			fmt.Println("receive buf err", err)
+		//buf := make([]byte, utils.GlobalObject.MaxPackageSize)
+		////cnt, err := c.Conn.Read(buf)
+		//_, err := c.Conn.Read(buf)
+		//if err != nil {
+		//	fmt.Println("receive buf err", err)
+		//	break
+		//}
+		// 创建一个拆包解包对象
+		dp := NewDataPack()
+
+		// 读取客户端的Msg Head 二进制流 8个字节
+		headData := make([]byte, dp.GetHeadLen())
+		if _, err := io.ReadFull(c.GetTCPConnection(), headData); err != nil {
+			fmt.Println("read msg head error", err)
 			break
 		}
+		// 拆包，得到MsgID和MsgDataLen 放在msg消息中
+		msg, err := dp.Unpack(headData)
+		if err != nil {
+			fmt.Println("unpack error", err)
+			break
+		}
+
+		// 根据dataLen 再次读取Data，放在msg.Data中
+		var data []byte
+		if msg.GetMsgLen() > 0 {
+			data = make([]byte, msg.GetMsgLen())
+			if _, err := io.ReadFull(c.GetTCPConnection(), data); err != nil {
+				fmt.Println("read msg data error", err)
+				break
+			}
+		}
+		msg.SetData(data)
 
 		//// 调用当前链接所绑定的handleAPI
 		//if err := c.handleAPI(c.Conn, buf, cnt); err != nil {
@@ -68,7 +95,7 @@ func (c *Connection) StartReader() {
 		// 得到当前conn数据的Request请求数据
 		req := Request{
 			conn: c,
-			data: buf,
+			msg:  msg,
 		}
 
 		go func(request ziface.IRequest) {
@@ -121,7 +148,26 @@ func (c *Connection) RemoteAddr() net.Addr {
 	return c.Conn.RemoteAddr()
 }
 
-// 发送数据，将数据发送给远程的
-func (c *Connection) Send(data []byte) error {
+// 提供一个SendMsg方法 将我们要发送给客户端的数据，先进行封包，再发送
+func (c *Connection) SendMsg(msgId uint32, data []byte) error {
+	if c.isClosed == true {
+		return errors.New("Connection closed when send msg")
+	}
+
+	// 将data进行封包 MsgDataLen｜MsgID｜Data
+	dp := NewDataPack()
+
+	binaryMsg, err := dp.Pack(NewMsgPackage(msgId, data))
+	if err != nil {
+		fmt.Println("Pack error msg id = ", msgId)
+		return errors.New("Pack error msg")
+	}
+
+	// 将数据发送给客户端
+	if _, err := c.Conn.Write(binaryMsg); err != nil {
+		fmt.Println("Write msg id ", msgId, " error :", err)
+		return errors.New("conn Write error")
+	}
+
 	return nil
 }
